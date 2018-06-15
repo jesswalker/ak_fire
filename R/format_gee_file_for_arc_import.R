2dea14889154be8d552b896249ba53b9a80fc9bc
Walker <jesswalker@users.noreply.github.com>
2018-06-14 17:30:22 -0700

    Added documentation

--- R/format_gee_file_for_arc_import.R
+++ R/format_gee_file_for_arc_import.R
@@ -1,14 +1,38 @@
+########################################################################### #
+# 
+# format_gee_file_for_arc_import.R
+#
+# This helper module imports the csv files that result from running the script
+# AK_FRP in GEE. It reformats column headings so they can be read in Arc; 
+# output files can be then processed via the python script 
+# intersect_frp_data_with_fire_perimeters.py
+#
+# Rbinding into a single file creates a world of trouble for Arc, hence the 
+# multiple-file output.
+#
+# Input: D:/projects/ak_fire/data/tables/gee_output + files of mxd14a1_ak_GEE_yyyy.csv
+# Output: D:/projects/ak_fire/data/tables/gee_output + files of mxd14a1_ak_GEE_yyyy_processed.csv
+#
+########################################################################### #
+
+
+# Set path info
 path.in = "D:/projects/ak_fire/data/tables/gee_output"
 filenames.in <- list.files(path.in, pattern = "*.csv")
 
-x <- do.call(rbind, lapply(file.path(path.in, filenames.in), read.csv, header=T))
+# Process each file
+for (filename.in in filenames.in) {
+  
+  x <- read.csv(file.path(path.in, filename.in), header = T)
+  filename.sub <- substr(basename(filename.in), 1, nchar(basename(filename.in))-4)
+  filename.out <- paste0(filename.sub, "_processed.csv")
 
 # rename first column for convenience
-colnames(x)[1] <- "index"
+  colnames(x)[1] <- "index"
  
 # R slaps an "X" in front of numerical columns; switch it to "class"
-names.sub <- names(x)[which((substring(names(x), 1, 1) == "X"))]
-names(x)[which((substring(names(x), 1,1) == "X"))] <- paste0("class", substring(names.sub, 2))
+  names.sub <- names(x)[which((substring(names(x), 1, 1) == "X"))]
+  names(x)[which((substring(names(x), 1,1) == "X"))] <- paste0("class", substring(names.sub, 2))
 
 # drop .geo column
 if (".geo" %in% names(x)) {
@@ -20,10 +44,25 @@ if ("histogram" %in% names(x)) {
   x$histogram <- NULL
 }
 
+# drop label column
+if ("label" %in% names(x)) {
+  x$label <- NULL
+}
+
+# drop date column
+if ("date" %in% names(x)) {
+  x$date <- NULL
+}
+
+# drop lonID column
+if ("lonID" %in% names(x)) {
+  x$lonID <- NULL
+}
+
 # drop 1st row, which is blank (1 exists for each file)
 x <- x[-1, ]
 
-# save file
-filename.out = "mxd14a1_ak_GEE_all.csv"
+# save output
 write.csv(x, file = file.path(path.in, filename.out), row.names = FALSE)
+}
 
